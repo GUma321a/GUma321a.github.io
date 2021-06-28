cd15108e940fc3b4c4ae1cd85887bc17501a5043
GUma321a <alexandrchernicov@gmail.com>
2021-06-28 17:50:49 +0300

    fix burgermenu

--- seatcovers/app/js/main.js
+++ seatcovers/app/js/main.js
@@ -47,9 +47,18 @@ const swiper = new Swiper('.swiper-container', {
     slidesPerView: 3,
     loop: true,
     centeredSlides: true,
-    brealpoints:{
-      1250: {
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        centeredSlides: false
+      },
+      580: {
         slidesPerView: 2,
+        centeredSlides: false
+      },
+      1150: {
+        slidesPerView: 3,
+        centeredSlides: true
       }
     },
     // autoplay: {
@@ -78,12 +87,55 @@ const swiper = new Swiper('.swiper-container', {
 //////////////////////////////////////////////////////////////
 var mixer = mixitup('.catalog__list');
 //////////////////////////////////////////////////////////////
-const burgerMenu = document.querySelector('.header__menu-burger');
-if (burgerMenu) {
-  const headerMenu = document.querySelector('.header__menu-items');
-  burgerMenu.addEventListener("click", function (e) {
-    document.body.classList.toggle('_lock');
-    burgerMenu.classList.toggle('_active');
-    headerMenu.classList.toggle('_active');
-  });
+// Мобильное меню бургер
+function burgerMenu() {
+  let burger = document.querySelector('.header__menu-burger')
+  let menu = document.querySelector('.header__menu-items')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+    }
+  })
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 999) {
+        menu.classList.remove('active')
+        burger.classList.remove('active-burger')
+    }
+  })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+const nav = document.querySelector('nav')
+
+// тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+const breakpoint = 500 
+if ( window.scrollY >= breakpoint) {
+  nav.classList.add('fixed__nav')
+} else {
+  nav.classList.remove('fixed__nav')
+}
+}
+// window.addEventListener('scroll', fixedNav)
+/////////////////////////////////////////////////////////////
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
 }
